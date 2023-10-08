71db09e42743078a9f90ff64e23611c490481447
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-08 12:15:53 -0700

    mod

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "{}{}{}".format(self.__dict__, self.__class__.__name__, self.id)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+dada = BaseModel()
+print (dada)
+print (dada.created_at)
+obj_dict = dada.to_dict()
+print (obj_dict)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+ print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
