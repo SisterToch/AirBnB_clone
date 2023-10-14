14c26dc5903b7dd0ba00721a46f1a4cfc812fa61
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-14 12:40:53 -0700

    as

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,13 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    """this is the base class"""
 
     def __init__(self, *args, **kwargs):
+        """the definition"""
         from models import storage
 
         if kwargs:
@@ -25,15 +27,18 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
+        """the string version"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """saves the instance"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
+        """handles the key and value"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
