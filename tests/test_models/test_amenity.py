70a60e1368735916ebc88eece6fb8366ae6ad47f
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-15 16:29:50 -0700

    add

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
-"""this module is for amenity"""
+"""
+this module is for amenity
+"""
 import unittest
 from models.amenity import Amenity
 
 
 class AmenityTest(unittest.TestCase):
-    """this class is for testing the amenity"""
+    """
+    this class is for testing the amenity code
+    """
     def test_attribute_type(self):
-        """test the class here"""
+        """
+        test the class here via the def
+        """
         amenity = Amenity()
         self.assertEqual(type(amenity.name), str)
+
+    def test_inheritance(self):
+        """
+        this checks if Amenity does inherits from BaseModel.
+        """
+        self.assertTrue(issubclass(Amenity, BaseModel))
+
+    def test_instance_exist(self):
+        """
+        this functions checks if the amenity object exists.
+        """
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+
+
+if __name__ == '__main__':
+    unittest.main()
